be229b21b4e148b9a32935a0c87b2dd4ac57dd37
juliayara <julia.yarahuan@gmail.com>
2022-10-19 16:16:55 -0400

    train/test split

---
+++ Test_Train_Split.R
@@ -0,0 +1,13 @@
+#File path variable: Input your file path here
+abxfile="/Volumes/dmcommon/_DM.Private/CI Program Faculty/+Fellow Franchise/Yarahuan/Protected/Antibiotics.csv"
+culturefile="/Volumes/dmcommon/_DM.Private/CI Program Faculty/+Fellow Franchise/Yarahuan/Antibiogram/Automated Algorithm//Positive Blood or Respiratory Cultures with organism.csv"
+
+#Load Datasets
+abxdata <- read_csv(abxfile)
+csnlistforsample<-unique(abxdata$`Contact Serial Number`)
+length(csnlistforsample)
+csnlistTRAIN<-sample(csnlistforsample,1000)
+
+#save CSN list as CSV
+
+write.csv(csnlist,"/Volumes/dmcommon/_DM.Private/CI Program Faculty/+Fellow Franchise/Yarahuan/Protected/CSNList.csv", row.names = FALSE)
